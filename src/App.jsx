ee1f950574db1151ef9d0e3652236144e559224a
azhari hsb <azhari.hsb27@gmail.com>
2024-06-20 22:30:49 +0700

    adding tiny changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-center">Hello World</h1>
-    </>
-  );
+  return <div className="min-h-screen bg-gradient"></div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    backgroundImage: {
+      gradient: 'linear-gradient(to top, #e6e9f0 0%, #eef1f5 100%);',
+    },
     extend: {},
   },
   plugins: [],
